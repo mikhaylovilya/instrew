03f9aa6b58d06bd090e2c8f85118e77bada498f3
Ilya Mikhaylov <ilyamikh121991@gmail.com>
2024-11-30 23:22:18 +0000

    client: fix CALL_PLT relocation for rv64

--- test/riscv64/build-rec.sh
+++ test/riscv64/build-rec.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 gcc -Wall -Wpedantic -static -no-pie rec.c -o rec
 gcc -Wall -Wpedantic -static -no-pie -fno-pie -nostdlib rec_nolibc.c -o rec_nolibc
+
+gcc -Wall -Wpedantic -static -nostdlib -no-pie -fno-pie -fno-pic exit.S -o exit
+gcc -Wall -Wpedantic -static -nostdlib  exit.S -o exit-pie
