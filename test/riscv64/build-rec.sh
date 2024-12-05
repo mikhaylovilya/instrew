abf011edc98d766264dc4af5fa9c092b6c883529
Ilya Mikhaylov <ilyamikh121991@gmail.com>
2024-12-05 11:55:43 +0000

    client: add plt entries

--- test/riscv64/build-rec.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-gcc -Wall -Wpedantic -static rec.c -o rec
-gcc -Wall -Wpedantic -static -no-pie -fno-pie -nostdlib rec_nolibc.c -o rec_nolibc
-
-gcc -Wall -Wpedantic -static -nostdlib -no-pie -fno-pie -fno-pic exit.S -o exit
-gcc -Wall -Wpedantic -static -nostdlib  exit.S -o exit-pie
