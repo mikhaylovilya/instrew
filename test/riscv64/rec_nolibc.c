13b0b05dad24e1616779838b8d8c96dd3170f389
Ilya Mikhaylov <ily2407@gmail.com>
2025-02-18 14:11:59 +0000

    client: cleaning & refactoring

--- test/riscv64/rec_nolibc.c
+++ test/riscv64/rec_nolibc.c
@@ -1,6 +1,3 @@
-//#include <stdio.h>    
-//#include <stdint.h>    
-     
 int rec(int n) {    
     if (n <= 0)    
         return 1;    
@@ -10,7 +7,6 @@ int rec(int n) {
 __attribute__((force_align_arg_pointer))    
 void _start() {    
     int n = 5;    
-//  printf("rec(%d) = %d\n", n, rec(n));    
     int res = rec(n);    
     asm("lb a0, %0\n"    
         "li a7, 93\n"    
